0ec2c936597a5a30701d9a4b84be47b898715b85
Jynic <ivanozefanya29@gmail.com>
2023-12-22 17:42:34 +0700

    BASE

---
+++ app/src/main/java/com/ivano/native_uts/CardCerbung.kt
@@ -0,0 +1,11 @@
+package com.ivano.native_uts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardCerbung : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_cerbung)
+    }
+}
---
+++ app/src/main/java/com/ivano/native_uts/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.ivano.native_uts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ivano.native_uts.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
